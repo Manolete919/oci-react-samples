411d7a59943e1cb4eafb09d2356b60b0ac74c240
manolete919 <manolete919@gmail.com>
2023-07-24 15:58:56 -0500

    ; added

--- mtdrworkshop/backend/src/main/java/com/oracle/todoapp/Main.java
+++ mtdrworkshop/backend/src/main/java/com/oracle/todoapp/Main.java
@@ -27,7 +27,7 @@ public final class Main {
   public static void main(final String[] args)
       throws IOException, SQLException {
     System.out.println("Working Directory = " + System.getProperty("user.dir"));
-    System.out.println("testing properties database.url " + config.get("database.url"))
+    System.out.println("testing properties database.url " + config.get("database.url"));
     
     System.setProperty("oracle.jdbc.fanEnabled", "false");
     LogManager
